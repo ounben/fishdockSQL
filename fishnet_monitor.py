c53a7fd4a64d35de7cd49377201e4efde5e6e782
ounben <48635796+ounben@users.noreply.github.com>
2026-02-03 22:05:35 +0100

    Modify DB connection settings and enhance metrics logging
    Updated database connection parameters and added new fields for metrics and worker stats tables. Enhanced error handling and logging for better debugging.

--- fishnet_monitor.py
+++ fishnet_monitor.py
@@ -12,12 +12,12 @@ import urllib.request  # Standard: Um Webseiten aufzurufen
 # --- Konfiguration ---
 # DB_HOST = os.environ.get('DB_HOST', 'postgres_db')
 # DB_NAME = os.environ.get('DB_NAME', 'fishnet_stats')
-# DB_USER = os.environ.get('DB_USER', '')
-# DB_PASS = os.environ.get('DB_PASS', '')#.strip()
+# DB_USER = os.environ.get('DB_USER', 'fish_admin')
+# DB_PASS = os.environ.get('DB_PASS', 'PASSWORT')#.strip()
 DB_HOST = 'postgres_db' 
 DB_NAME = 'fishnet_stats'
-DB_USER = ''
-DB_PASS = '' # Direkt als String
+DB_USER = 'fish_admin'
+DB_PASS = 'PASSWORT' # Direkt als String
 
 # Regex für Lichess-Logs
 LOG_PATTERN = re.compile(r"https://lichess\.org/(?P<id>[A-Za-z0-9_-]+).+finished\s+\((?P<knps>\d+)\s+knps/core\)")
@@ -29,7 +29,7 @@ def get_db_connection():
             print("Datenbank verbunden!")
             return conn
         except Exception as e:
-            # Hier siehst du jetzt den echten Grund (z.B. "password authentication failed" oder "could not translate host name")
+            # Grund
             print(f"Verbindungsfehler: {e}") 
             print("Warte auf SQL-Datenbank...")
             time.sleep(5)
@@ -37,21 +37,22 @@ def get_db_connection():
 conn = get_db_connection()
 cur = conn.cursor()
 
-# Tabelle mit ALLEN Feldern initialisieren
+# Tabelle mit ALLEN Feldern 
 cur.execute("""
     CREATE TABLE IF NOT EXISTS metrics (
         id SERIAL PRIMARY KEY,
         time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         container_name TEXT, status VARCHAR(2), error_msg TEXT, 
         batch_id TEXT, analysis_type TEXT, variant TEXT, nps_si BIGINT, cores INTEGER, version TEXT,
-        pbo_ppt_watt FLOAT, pbo_edc_ampere FLOAT, temp_c FLOAT,chess_com_players BIGINT,
+        pbo_ppt_watt FLOAT, pbo_edc_ampere FLOAT, temp_c FLOAT,chess_com_players BIGINT,  lichess_ping FLOAT, chess_com_ping FLOAT,
         lichess_online INTEGER, lichess_games INTEGER,
         user_acquired INTEGER, user_queued INTEGER, user_oldest INTEGER,
         system_acquired INTEGER, system_queued INTEGER, system_oldest INTEGER
     );
 """)
 conn.commit()
-# Tabelle für den Reset-Schutz und kumulative Stats
+
+# Tabelle Worker_stats
 cur.execute("""
     CREATE TABLE IF NOT EXISTS worker_stats (
         id SERIAL PRIMARY KEY,
@@ -69,11 +70,11 @@ conn.commit()
 
 client = docker.from_env()
 
-# Globaler Cache für Lichess-Werte (Startwerte None)
+# Globaler Cache für Lichess-Werte (Startwert NONE)
 cached_stats = {
     "players": None, "games": None, 
     "u_acq": None, "u_que": None, "u_old": None,
-    "s_acq": None, "s_que": None, "s_old": None, "chess_com_players": None,
+    "s_acq": None, "s_que": None, "s_old": None, "chess_com_players": None, "lichess_ping": None, "chess_com_ping": None,
     "last_update": 0
 }
 monitored_containers = set()
@@ -85,51 +86,56 @@ def extract_json_data(text, pattern):
 # Chess.com Players
 
 def fetch_chess_com_players():
-    # Auf der Seite suchen
+    # Seite mit dem Spieler Wert ist nicht Startpage
     url = "https://www.chess.com/play/online"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
-    
+    start_time = time.time()
     try:
         req = urllib.request.Request(url, headers=headers)
-        with urllib.request.urlopen(req, timeout=10) as response:
+        with urllib.request.urlopen(req, timeout=5) as response:
             html_text = response.read().decode('utf-8')
-        
-        # Regex:
+            ping = time.time() - start_time # Zeit in Sekunden (SI)
+        # Dieser Regex sucht exakt nach der Variable HTML
         # window.chesscom.statsMembersOnline = 215862;
         js_match = re.search(r'statsMembersOnline\s*=\s*(\d+)', html_text)
         if js_match:
-            return int(js_match.group(1))
+            return int(js_match.group(1)), ping
 
         # Zweiter Versuch
         meta_match = re.search(r'(\d{1,3}(?:[.,]\d{3})+) users online', html_text, re.IGNORECASE)
         if meta_match:
-            return int(''.join(filter(str.isdigit, meta_match.group(1))))
+            return int(''.join(filter(str.isdigit, meta_match.group(1)))), ping
             
     except Exception as e:
         print(f"[!] Chess.com Scrape Fehler: {e}")
-    return None
-
+    return None, None # None
 def update_lichess_stats_worker():
     global cached_stats
-    # Vorgaukeln
+    # Browser vorgaukeln
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
 
     while True:
         try:
-            # 1. Hauptseite 
+            # Hauptseite 
             # Chess.com Update
-            new_chess_val = fetch_chess_com_players()
-            if new_chess_val:
-                cached_stats["chess_com_players"] = new_chess_val
-                print(f"[OK] Chess.com Sync: {new_chess_val} Players")
-            
+            # new_chess_val = fetch_chess_com_players()
+            # if new_chess_val:
+                # cached_stats["chess_com_players"] = new_chess_val
+                # cached_stats["chess_com_ping"] = chess_ping # Ping oder None
+                # print(f"[OK] Chess.com Sync: {new_chess_val} Players Ping {new_chess_val}" )
+            res_chess, ping_chess = fetch_chess_com_players()
+            if res_chess is not None:
+                cached_stats["chess_com_players"] = res_chess
+                cached_stats["chess_com_ping"] = ping_chess
+                print(f"[OK] Chess.com Sync: {res_chess} Players | Ping: {ping_chess:.3f}s")
+                
             r1 = requests.get("https://lichess.org/", headers=headers, timeout=5)
-            
-            # Suche
+            cached_stats["lichess_ping"] = r1.elapsed.total_seconds()
+            # Suche nach  Wort in HTML
             players_match = re.search(r'"members":(\d+)', r1.text)
             games_match = re.search(r'"rounds":(\d+)', r1.text)
             
@@ -139,7 +145,6 @@ def update_lichess_stats_worker():
                 cached_stats["games"] = int(games_match.group(1))
 
             # 2. Fishnet Status (Queues)
-            # Regex
             r2 = requests.get("https://lichess.org/fishnet/status", headers=headers, timeout=5)
             d2 = extract_json_data(r2.text, r'{"analysis":{"user":{.*?},"system":{.*?}}}')
             if d2:
@@ -152,10 +157,11 @@ def update_lichess_stats_worker():
             cached_stats["last_update"] = time.time()
             
         except Exception as e:
-            # Falls es doch mal hakt, siehst du es hier
+            # 
             print(f"[!] Lichess Update Fehler (Python): {e}")
-        
-        time.sleep(60)# Update  60 
+            cached_stats["lichess_ping"] = None
+            cached_stats["chess_com_ping"] = None
+        time.sleep(60)# Update  60 Sekunden
 
 def get_pbo_data():
     stats = {"ppt": None, "edc": None, "temp": None}
@@ -198,7 +204,7 @@ def stream_logs(container_name):
             msg = line.decode('utf-8', errors='ignore').strip()
             if not msg: continue
 
-            # --- HEADER ---
+            # --- HEADER EXTRAKTION (Kumulative Werte & Reset-Schutz) ---
             if "total nodes" in msg and "><>" in msg:
                 h_match = re.search(r'><>\s+v([\d\.]+):.*?([\d\.]+)\s+batches,\s+([\d\.]+)\s+positions,\s+([\d\.]+)\s+total nodes', msg)
                 if h_match:
@@ -219,7 +225,7 @@ def stream_logs(container_name):
                         if row and n_total < row[0]:
                             print(f"[!!!] RESET erkannt bei {container_name}: {n_total} < {row[0]}. Repariere...")
                             
-                            # JSON-Backup nach deinem Format
+                            # JSON-Backup 
                             stats_json = {
                                 "total_batches": row[1],
                                 "total_positions": row[2],
@@ -236,7 +242,7 @@ def stream_logs(container_name):
                             print(f"[#] {container_name} mit alten Werten neu gestartet.")
                             return # Thread beenden, main() übernimmt nach Restart
 
-                        # Normaler Insert
+                        # Normaler Insert wenn kein Reset 
                         cur.execute("""
                             INSERT INTO worker_stats (
                                 time, container_name, version, 
@@ -251,32 +257,31 @@ def stream_logs(container_name):
                         conn.rollback()
                         print(f"Fehler im Header-Prozess ({container_name}): {e}")
 
-            # Initialisierung Fallback 
+            # --- Initialisierung & Fallback ---
             status, error_msg, nps_si, batch_id = None, None, None, None
             analysis_type, variant = None, None
 
-            # Fehler
+            # 1: Fehler/Warnungen 
             if msg.startswith(("W:", "E:")):
                 parts = msg.split(":", 1)
                 status = parts[0].strip()
                 error_msg = parts[1].strip()
 
-            # fishnet finished
-            elif "finished" in msg:
+            # 2: Erfolge 
+                elif "finished" in msg:
                 status = "OK"
                 id_match = re.search(r'(?:https://lichess\.org/(\w+)|batch (\w+))', msg)
                 if id_match:
                     batch_id = id_match.group(1) or id_match.group(2)
                     analysis_type = "USER" if "https" in msg else "SYSTEM"
                 
-                # Variante NPS
-                # Erkennt optional die Variante und zwingend die knps/core
+                # 4. Variante und NPS extrahieren
                 #variant_match = re.search(r'\((?:([\w\d]+),\s+)?(\d+)\s+knps/core\)', msg)
                 #variant_match = re.search(r'\((?:([a-zA-Z]+),\s+)?(?:[\w\d]+,\s+)?(\d+)\s+knps/core\)', msg)
                 variant_match = re.search(r'\((?:([\w\d]+),\s+)?(?:[\w\d]+,\s+)?(\d+)\s+knps/core\)', msg)
                 
                 if variant_match:
-                    # Falls Gruppe 1 leer nur knps im Log), setze "standard"
+                    # Falls Gruppe 1 leer ist (nur knps im Log), setze "standard"
                     variant = variant_match.group(1) if variant_match.group(1) else "standard"
                     # Umrechnung in nps
                     nps_si = int(variant_match.group(2)) * 1000
@@ -284,7 +289,7 @@ def stream_logs(container_name):
                     # Fallback
                     variant = None
                     
-            # Speichern (OK, W , E)
+            # 3: Speichern (nur wenn OK, W oder E erkannt wurde) 
             if status:
                 pbo = get_pbo_data()
                 l = cached_stats
@@ -292,19 +297,39 @@ def stream_logs(container_name):
                     cur.execute("""
                         INSERT INTO metrics (
                             container_name, status, error_msg, batch_id, analysis_type, variant, nps_si, cores, version, 
-                            pbo_ppt_watt, pbo_edc_ampere, temp_c, chess_com_players, lichess_online, lichess_games,
+                            pbo_ppt_watt, pbo_edc_ampere, temp_c, chess_com_players, lichess_ping, chess_com_ping, lichess_online, lichess_games,
                             user_acquired, user_queued, user_oldest, system_acquired, system_queued, system_oldest
-                        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     """, (
-                        container_name, status, error_msg, batch_id, analysis_type, variant, nps_si, cores, current_version,
-                        pbo['ppt'], pbo['edc'], pbo['temp'], l['chess_com_players'],  l['players'], l['games'],
-                        l['u_acq'], l['u_que'], l['u_old'], l['s_acq'], l['s_que'], l['s_old']
+                        container_name, 
+                        status, 
+                        error_msg, 
+                        batch_id, 
+                        analysis_type, 
+                        variant, 
+                        nps_si, 
+                        cores, 
+                        current_version,
+                        pbo['ppt'], 
+                        pbo['edc'], 
+                        pbo['temp'], 
+                        l['chess_com_players'],  
+                        l['lichess_ping'],      
+                        l['chess_com_ping'],     
+                        l['players'],            
+                        l['games'],              
+                        l['u_acq'], 
+                        l['u_que'], 
+                        l['u_old'], 
+                        l['s_acq'], 
+                        l['s_que'], 
+                        l['s_old']
                     ))
                     conn.commit()
                 except Exception as db_err:
                     conn.rollback()
                 
-                print(f"[{container_name}] ID:{batch_id} | {analysis_type} | {status} | {error_msg} | {variant} | {nps_si}nps | {cores}C | {current_version} | {pbo['temp']}C | {pbo['ppt']}W | {pbo['edc']}A | ChessCom: {cached_stats['chess_com_players']}u | L-Stat:{cached_stats['players']}u/{cached_stats['games']}g | User(A/Q/O):{cached_stats['u_acq']}/{cached_stats['u_que']}/{cached_stats['u_old']}s | Sys(A/Q/O):{cached_stats['s_acq']}/{cached_stats['s_que']}/{cached_stats['s_old']}s")
+                print(f"[{container_name}] ID:{batch_id} | {analysis_type} | {status} | {error_msg} | {variant} | {nps_si}nps | {cores}C | {current_version} | {pbo['temp']}C | {pbo['ppt']}W | {pbo['edc']}A | Chesscom: {cached_stats['chess_com_players']}u {cached_stats['chess_com_ping']:.3f}s | L-Stat:{cached_stats['players']}u/{cached_stats['games']}g {cached_stats['lichess_ping']:.3f}s | User(A/Q/O):{cached_stats['u_acq']}/{cached_stats['u_que']}/{cached_stats['u_old']}s | Sys(A/Q/O):{cached_stats['s_acq']}/{cached_stats['s_que']}/{cached_stats['s_old']}s")
                 
                 #print(f"[{container_name}] ID:{batch_id} | {analysis_type} | {status} | {variant} | {nps_si}nps | {cores}C | {pbo['temp']}C | {pbo['ppt']}W | {pbo['edc']}A | User(A/Q/O):{cached_stats['u_acq']}/{cached_stats['u_que']}/{cached_stats['u_old']}s")
 
@@ -314,7 +339,7 @@ def stream_logs(container_name):
         monitored_containers.discard(container_name)
 
 def main():
-    # Lichess-Hintergrund-Thread
+    # Start
     threading.Thread(target=update_lichess_stats_worker, daemon=True).start()
     
     print("Starte SI-Logger mit Fishnet-Queue-Analyse...")
